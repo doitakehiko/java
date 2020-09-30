376ac0c791f55e2f84bbff81de72ef4f3bac25e9
doitakehiko <takehiko.doi@gmail.com>
2020-09-30 10:14:28 +0900

    ファイル名を消す

--- hon_java.bat
+++ hon_java.bat
@@ -1,3 +1,3 @@
 SET $OutputEncoding = [Console]::OutputEncoding
-find " " D:\doc\manga?.txt | call java MoveTailToHead | call sort > D:\doc\manga_res.txt
-find " " D:\doc\lightnovel?.txt |  call java MoveTailToHead | call sort > D:\doc\lightnovel_res.txt
+type D:\doc\manga?.txt | find " " |  call java MoveTailToHead | call sort > D:\doc\manga_res.txt
+type D:\doc\lightnovel?.txt  | find " "  | call java MoveTailToHead | call sort > D:\doc\lightnovel_res.txt
