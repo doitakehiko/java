b8d0064f7ad2abb12c192f9234ef6ba1ddbe4114
doitakehiko <takehiko.doi@gmail.com>
2025-07-12 08:00:35 +0900

    IfWhileJudgment

---
+++ IfWhileJudgment.java
@@ -0,0 +1,7 @@
+public class IfWhileJudgment {
+    public static void main (String[] args) {
+	if ( 0 ) {
+		System.out.println("compilation error");
+	}
+    }
+}
---
+++ ifTest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	if ( 0 ) {
+		printf("0 is true.\n");
+	} else {
+		printf("0 is false.\n");
+	}
+
+	if ( 1 ) {
+		printf("1 is true.\n");
+	} else {
+		printf("1 is false.\n");
+	}
+
+
+	if ( -1 ) {
+		printf("-1 is true.\n");
+	} else {
+		printf("-1 is false.\n");
+	}
+
+
+	return 0;
+}
